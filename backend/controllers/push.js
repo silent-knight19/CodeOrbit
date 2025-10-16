a1cd9a3eb31a994e126d42ebce985905708ea698
silent-knight19 <sachinsinghtomar7749@gmail.com>
2025-10-16 20:32:20 +0530

    day 4 hour 1

--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -1,4 +1,35 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { s3, s3_bucket } = require("../config/aws-config");
+
 async function pushChanges() {
-    console.log("Pushing changes to the remote repository");
+    const repoPath = path.resolve(process.cwd(), ".MyGit");
+
+    const commitsPath = path.join(repoPath, "commits");
+    try {
+        const commitDirs = await fs.readdir(commitsPath);
+        for (const commitDir of commitDirs) {
+
+            const commitpath = path.join(commitsPath,commitDir);
+            const fileContent = await fs.readdir(commitpath);
+
+            for (const file of fileContent) 
+                {
+                const filePath = path.join(commitpath,file);
+                const fileContent = await fs.readFile(filePath);
+                const params = {
+                    Bucket: s3_bucket,
+                    Key: `commit/${commitDir}/${file}`,
+                    Body: fileContent,
+                };
+                await s3.upload(params).promise();
+                
+            }
+
+        }
+        console.log("Changes pushed to remote repository");
+    } catch (err) {
+        console.error("Error pushing changes", err);
+    }
 }
 module.exports={pushChanges};
